7f465457e8324caf408716fc657ae4aa20d987d4
Dzmitry Liashko <dzliashko@gmail.com>
2025-08-25 16:10:44 +0300

    Add: while

--- src/main/java/com/codewithjava21/chapter2/RandomCase.java
+++ src/main/java/com/codewithjava21/chapter2/RandomCase.java
@@ -5,15 +5,18 @@ import java.util.Random;
 public class RandomCase {
     public static void main(String[] args) {
         Random random = new Random();
-
+        boolean fourFound = false;
         String result = "";
-        for (int i = 0; i < 10; i++) {
+        while (!fourFound) {
             int number = random.nextInt(5) + 1;
             switch (number) {
                 case 1 -> result = "One";
                 case 2 -> result = "Two";
                 case 3 -> result = "Three";
-                case 4 -> result = "Four";
+                case 4 -> {
+                    result = "Four";
+                    fourFound = true;
+                }
                 case 5 -> result = "Five";
                 default -> result = "Something strange";
             }
