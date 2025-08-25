5f9db9746019f66718055111330de5b4e330e28b
Dzmitry Liashko <dzliashko@gmail.com>
2025-08-25 15:42:00 +0300

    Add: read input

--- src/main/java/com/codewithjava21/chapter2/ReadingInput.java
+++ src/main/java/com/codewithjava21/chapter2/ReadingInput.java
@@ -1,4 +1,15 @@
 package com.codewithjava21.chapter2;
 
+import java.util.Scanner;
+
 public class ReadingInput {
+    public static void main(String[] args) {
+        System.out.print("Enter a number from 31 to 256: ");
+        Scanner inputScanner = new Scanner(System.in);
+        String inputString = inputScanner.nextLine();
+        inputScanner.close();
+
+        int number = Integer.parseInt(inputString);
+        System.out.printf("Symbol for ASCII code %d - is %c", number, (char) number);
+    }
 }
