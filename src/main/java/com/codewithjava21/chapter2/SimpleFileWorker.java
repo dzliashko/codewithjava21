a9824d60a7171d21881098780dc8cd6c38fafba1
Dzmitry Liashko <dzliashko@gmail.com>
2025-08-25 16:31:15 +0300

    Add: BufferedWriter

--- src/main/java/com/codewithjava21/chapter2/SimpleFileWorker.java
+++ src/main/java/com/codewithjava21/chapter2/SimpleFileWorker.java
@@ -1,16 +1,20 @@
 package com.codewithjava21.chapter2;
 
-import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class SimpleFileWorker {
     public static void main(String[] args) {
         try {
-            FileWriter writer = new FileWriter("gamesCatalog.txt");
+            BufferedWriter writer = new BufferedWriter(new FileWriter("gamesCatalog.txt"));
             writer.write("name, company, year\n");
             writer.write("Pitfall!, Activision, 1982\n");
+            writer.write("Crackpots, Activision, 1983\n");
+            writer.write("Yar's Revenge, Atari, 1981\n");
+            writer.write("Warlords, Atari, 1981\n");
+            writer.write("Defender, Atari, 1981\n");
+            writer.write("Adventure, Atari, 1980\n");
             writer.close();
         } catch (IOException e) {
             System.out.println("Error writing to file");
