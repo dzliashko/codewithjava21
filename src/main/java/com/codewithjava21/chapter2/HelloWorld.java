3f89ea5146eeb1636a56eee15759dadd431233f0
Dzmitry Liashko <dzliashko@gmail.com>
2025-08-25 15:36:07 +0300

    Add: variables

--- src/main/java/com/codewithjava21/chapter2/HelloWorld.java
+++ src/main/java/com/codewithjava21/chapter2/HelloWorld.java
@@ -1,7 +1,12 @@
 package com.codewithjava21.chapter2;
 
 public class HelloWorld {
+    private static String firstName = "Dzmitry";
+    private static int age = 52;
+
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.printf("Welcome to the world of Java, %s!\n", firstName);
+        System.out.printf("age = %d. It's never to late to learn Java!\n", age);
     }
 }
