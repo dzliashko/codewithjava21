7564dd42f700d8a06c84f65a466ce01c745342ae
Dzmitry Liashko <dzliashko@gmail.com>
2025-09-05 22:25:58 +0300

    Add: WorkingWithCollections empty

---
+++ src/main/java/com/codewithjava21/chapter4/WorkingWithCollections.java
@@ -0,0 +1,16 @@
+package com.codewithjava21.chapter4;
+
+import java.util.Collection;
+
+public class WorkingWithCollections {
+    public static void main(String[] args) {
+
+    }
+
+    private static void printCollection(Collection collection) {
+        for (Object element : collection) {
+            System.out.printf("%s ", element.toString());
+            System.out.println();
+        }
+    }
+}
