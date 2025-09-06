7ac82087c4af3704738f3820c3c7ad66503c19d2
Dzmitry Liashko <dzliashko@gmail.com>
2025-09-06 12:15:40 +0300

    Add: WorkingWithCollectionsMaps TreeMap

--- src/main/java/com/codewithjava21/chapter4/WorkingWithCollectionsMaps.java
+++ src/main/java/com/codewithjava21/chapter4/WorkingWithCollectionsMaps.java
@@ -8,7 +8,9 @@ public class WorkingWithCollectionsMaps {
 //        Hashtable support interaction with multiple threads
 //        Map<String, String> spellBook = new Hashtable<>();
 //        LinkedHashMap support the order that they were added
-        Map<String, String> spellBook = new LinkedHashMap<>();
+//        Map<String, String> spellBook = new LinkedHashMap<>();
+//        TreeMap map has been sorted by the keys in alpha-numeric order
+        Map<String, String> spellBook = new TreeMap<>();
         spellBook.put("Fireball", "A ball of fire that inflicts 8 damage per level of magic.");
         spellBook.put("Healing Touch", "Touching an injured player recovers 5 hit points per character level.");
         spellBook.put("Lightning Bolt", "A stream of lightning that inflicts 10 damage per level of magic.");
