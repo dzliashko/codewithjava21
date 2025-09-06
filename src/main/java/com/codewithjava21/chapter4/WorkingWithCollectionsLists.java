d56d49bb72a6992030fed3ffc3f342ef929aaead
Dzmitry Liashko <dzliashko@gmail.com>
2025-09-06 11:55:15 +0300

    Refactor: separate Collections

--- src/main/java/com/codewithjava21/chapter4/WorkingWithCollectionsLists.java
+++ src/main/java/com/codewithjava21/chapter4/WorkingWithCollectionsLists.java
@@ -1,47 +1,31 @@
 package com.codewithjava21.chapter4;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
 
-public class WorkingWithCollections {
+public class WorkingWithCollectionsLists {
     public static void main(String[] args) {
-        String[] heroes = {"Byorki", "K'lar", "Tyrenni",
-                "Athena", "Jarrod"};
-
-/*//        Set<String> heroesSet = new HashSet<>();
-//        Set<String> heroesSet = new LinkedHashSet<>();
-        Set<String> heroesSet = new TreeSet<>();
-        Collections.addAll(heroesSet, heroes);
-        printCollection(heroesSet);
-        heroesSet.add("Byorki");
-        printCollection(heroesSet);
-//      in HashSet not support order of elements
-//      in LinkedHashSet support order of elements
-//      in TreeSet support alphanumerical order of elements
-        heroesSet.add("Rik");
-        printCollection(heroesSet);
-        heroesSet.remove("Rik");
-        printCollection(heroesSet);*/
-
-//        Working with Lists
-
-//        just ArrayList
+        //        Working with Lists
+        //        just ArrayList
         List<String> monsterList = new ArrayList<>();
-//        Vector same as ArrayList but thread-safe
-//        List<String> monsterList = new Vector<>();
+        //        Vector same as ArrayList but thread-safe
+        //        List<String> monsterList = new Vector<>();
         monsterList.add("Kobald");
         monsterList.add("Skeleton");
         monsterList.add("Zombie");
         monsterList.add("Rats");
         monsterList.add("Skeleton");
 
-//        in ArrayList and Vector
-//        printCollection(monsterList);
-//        monsterList.remove("Skeleton");
-//        monsterList.remove(2);
-//        printCollection(monsterList);
-//        System.out.println(monsterList.get(1));
+        //        in ArrayList and Vector
+        //        printCollection(monsterList);
+        //        monsterList.remove("Skeleton");
+        //        monsterList.remove(2);
+        //        printCollection(monsterList);
+        //        System.out.println(monsterList.get(1));
 
-//        LinkedList support order of elements
+        //        LinkedList support order of elements
         LinkedList<String> cityList = new LinkedList<>();
         cityList.add("Elddim");
         cityList.add("Crystwind");
@@ -64,9 +48,7 @@ public class WorkingWithCollections {
     private static void printCollection(Collection collection) {
         for (Object element : collection) {
             System.out.printf("%s ", element.toString());
-
         }
         System.out.println();
-
     }
 }
---
+++ src/main/java/com/codewithjava21/chapter4/WorkingWithCollectionsSets.java
@@ -0,0 +1,32 @@
+package com.codewithjava21.chapter4;
+
+import java.util.*;
+
+public class WorkingWithCollectionsSets {
+    public static void main(String[] args) {
+        String[] heroes = {"Byorki", "K'lar", "Tyrenni",
+                "Athena", "Jarrod"};
+
+        //        Set<String> heroesSet = new HashSet<>();
+        //        Set<String> heroesSet = new LinkedHashSet<>();
+        Set<String> heroesSet = new TreeSet<>();
+        Collections.addAll(heroesSet, heroes);
+        printCollection(heroesSet);
+        heroesSet.add("Byorki");
+        printCollection(heroesSet);
+        //      in HashSet not support order of elements
+        //      in LinkedHashSet support order of elements
+        //      in TreeSet support alphanumerical order of elements
+        heroesSet.add("Rik");
+        printCollection(heroesSet);
+        heroesSet.remove("Rik");
+        printCollection(heroesSet);
+    }
+
+    private static void printCollection(Collection collection) {
+        for (Object element : collection) {
+            System.out.printf("%s ", element.toString());
+        }
+        System.out.println();
+    }
+}
