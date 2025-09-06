ff93a3694f6e7a1c77732ec02e97da437d29c91b
Dzmitry Liashko <dzliashko@gmail.com>
2025-09-06 12:10:22 +0300

    Add: WorkingWithCollectionsMaps HashMap

--- src/main/java/com/codewithjava21/chapter4/WorkingWithCollectionsMaps.java
+++ src/main/java/com/codewithjava21/chapter4/WorkingWithCollectionsMaps.java
@@ -1,12 +1,20 @@
 package com.codewithjava21.chapter4;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
 public class WorkingWithCollectionsMaps {
     public static void main(String[] args) {
+        Map<String, String> spellBook = new HashMap<>();
+        spellBook.put("Fireball", "A ball of fire that inflicts 8 damage per level of magic.");
+        spellBook.put("Healing Touch", "Touching an injured player recovers 5 hit points per character level.");
+        spellBook.put("Lightning Bolt", "A stream of lightning that inflicts 10 damage per level of magic.");
+        spellBook.put("Create Water", "Creates 10 liters of water per level of magic.");
+        spellBook.put("Transmutation", "Converts common items into gold.");
 
-
+        // A HashMap
+        printMap(spellBook);
     }
 
     private static void printMap(Map map) {
