a40308326e037ee6f46cc17334d61409fd8f05f0
Dzmitry Liashko <dzliashko@gmail.com>
2025-09-08 21:13:33 +0300

    Add: MathExamples function add

---
+++ src/main/java/com/codewithjava21/chapter5/MathExamples.java
@@ -0,0 +1,13 @@
+package com.codewithjava21.chapter5;
+
+public class MathExamples {
+    public static void main(String[] args) {
+        int intNumA = 5;
+        int intNumB = 3;
+        System.out.printf("%d + %d = %d%n", intNumA, intNumB, add(intNumA, intNumB));
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
