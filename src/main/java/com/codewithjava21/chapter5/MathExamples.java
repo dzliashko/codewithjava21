d24962a1e879a946e002f37ca4e705a9d2e9d655
Dzmitry Liashko <dzliashko@gmail.com>
2025-09-08 21:44:24 +0300

    Add: MathExamples

--- src/main/java/com/codewithjava21/chapter5/MathExamples.java
+++ src/main/java/com/codewithjava21/chapter5/MathExamples.java
@@ -2,10 +2,14 @@ package com.codewithjava21.chapter5;
 
 public class MathExamples {
     public static void main(String[] args) {
-        int intNumA = 5;
+        int intNumA = 6;
         int intNumB = 3;
         System.out.printf("%d + %d = %d%n", intNumA, intNumB, add(intNumA, intNumB));
         System.out.printf("%d - %d = %d%n", intNumA, intNumB, subtract(intNumA, intNumB));
+        System.out.printf("%d * %d = %d%n", intNumA, intNumB, multiply(intNumA, intNumB));
+        System.out.printf("%d / %d = %d%n", intNumA, intNumB, divide(intNumA, intNumB));
+        System.out.printf("%d modulo %d = %d%n", intNumA, intNumB, modulo(intNumA, intNumB));
+        System.out.printf("%d pow %d = %d%n", intNumA, intNumB, exponent(intNumA, intNumB));
     }
 
     public static int add(int a, int b) {
@@ -14,5 +18,21 @@ public class MathExamples {
 
     public static int subtract(int a, int b) {
         return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static int divide(int a, int b) {
+        return a / b;
+    }
+
+    public static int modulo(int a, int b) {
+        return a % b;
+    }
+
+    public static int exponent(int a, int b) {
+        return (int) Math.pow(a, b);
     }
 }
