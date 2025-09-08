154284e8f181251b8251eba24f9bc5bd81553eee
Dzmitry Liashko <dzliashko@gmail.com>
2025-09-08 21:51:23 +0300

    Add: Item

---
+++ src/main/java/com/codewithjava21/chapter6/Item.java
@@ -0,0 +1,19 @@
+package com.codewithjava21.chapter6;
+
+public class Item {
+    private String name;
+
+    public Item() {
+    }
+
+    public Item(String name) {
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
