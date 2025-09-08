f1f280f21ffc06dd4aee0adffd995cdc88b94ef2
Dzmitry Liashko <dzliashko@gmail.com>
2025-09-08 22:02:39 +0300

    Add: Stack

---
+++ src/main/java/com/codewithjava21/chapter6/Stack.java
@@ -0,0 +1,58 @@
+package com.codewithjava21.chapter6;
+
+import java.util.Arrays;
+
+public class Stack {
+    private Item[] items;
+    private int maxCount;
+    private int stackCount = 0;
+
+    public Stack() {
+        this(10);
+    }
+
+    public Stack(int numItems) {
+        maxCount = numItems;
+        items = new Item[maxCount];
+    }
+
+    private void resizeItemsArray() {
+        maxCount = maxCount + 5;
+        Item[] newArray = new Item[maxCount];
+
+        for (int counter = 0; counter < stackCount; counter++) {
+            newArray[counter] = items[counter];
+        }
+        items = newArray;
+    }
+
+    public void push(Item item) {
+        if (stackCount + 1 >= maxCount) {
+            resizeItemsArray();
+        }
+        stackCount++;
+        items[stackCount - 1] = item;
+    }
+
+    public Item peek() {
+        return items[stackCount - 1];
+    }
+
+    public Item pop() {
+        Item returnItem = peek();
+        stackCount--;
+        return returnItem;
+    }
+
+    public int getStackCount() {
+        return stackCount;
+    }
+
+    @Override
+    public String toString() {
+        return "Stack{" +
+                "items=" + Arrays.toString(items) +
+                ", stackCount=" + stackCount +
+                '}';
+    }
+}
