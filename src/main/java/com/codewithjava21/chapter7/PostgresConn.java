7d983204633f65cf8a1ac3de3ff82f66ec2f4993
Dzmitry Liashko <dzliashko@gmail.com>
2025-09-09 08:06:43 +0300

    Add: PostgresConn

---
+++ src/main/java/com/codewithjava21/chapter7/PostgresConn.java
@@ -0,0 +1,33 @@
+package com.codewithjava21.chapter7;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class PostgresConn {
+    private Connection conn;
+
+    public PostgresConn(String url, String username, String password) {
+        connectToPostgres(url, username, password);
+    }
+
+    private void connectToPostgres(String url, String username, String password) {
+        try {
+            conn = DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void closePostgresConnection() {
+        try {
+            conn.close();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public Connection getConn() {
+        return this.conn;
+    }
+}
